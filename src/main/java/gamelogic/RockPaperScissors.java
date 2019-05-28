865dfb638e3102b99e8cf59d92944ec833de8b30
RahulRj09 <rahul18@navgurukul.org>
2019-05-28 11:18:02 +0530

    add. function getComputerInput

--- src/main/java/gamelogic/RockPaperScissors.java
+++ src/main/java/gamelogic/RockPaperScissors.java
@@ -18,15 +18,18 @@ public class RockPaperScissors {
         }
         return "sorry you lost";
     }
+    static String getComputerInput(List<String> computerData){
+        Random random = new Random();
+       return computerData.get(random.nextInt(computerData.size()));
+    }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("choose one from 'rock' , 'paper' , 'scissors' ");
         String userInput = input.nextLine().toLowerCase();
         List<String> computerData = Arrays.asList("rock", "paper", "scissors");
-        Random random = new Random();
         if (computerData.contains(userInput)) {
-            System.out.println(getGameResult(userInput, computerData.get(random.nextInt(computerData.size()))));
+            System.out.println(getGameResult(userInput, getComputerInput(computerData)));
         } else {
             System.out.println("invalid input");
         }
