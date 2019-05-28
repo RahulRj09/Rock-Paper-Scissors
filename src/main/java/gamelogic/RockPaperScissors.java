4712e4d67f37ae59f9ef46561c773cd0258cb499
RahulRj09 <rahul18@navgurukul.org>
2019-05-28 12:27:13 +0530

    play again without loop

--- src/main/java/gamelogic/RockPaperScissors.java
+++ src/main/java/gamelogic/RockPaperScissors.java
@@ -6,28 +6,49 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissors {
+    static List<String> computerData = Arrays.asList("rock", "paper", "scissors");
+
     static String getGameResult(String userInput, String computerInput) {
         if (userInput.equals(computerInput)) {
-            return "draw";
-        } else if(userInput.equals("scissors") && computerInput.equals("paper")){
-                return "you win";
-        }else if(userInput.equals("paper") && computerInput.equals("rock")){
-            return "you win";
-        }else if(userInput.equals("rock") && computerInput.equals("scissors")){
-            return "you win";
+            System.out.println("draw");
+        } else if (userInput.equals("scissors") && computerInput.equals("paper")) {
+            System.out.println("you win");
+        } else if (userInput.equals("paper") && computerInput.equals("rock")) {
+            System.out.println("you win");
+        } else if (userInput.equals("rock") && computerInput.equals("scissors")) {
+            System.out.println("you win");
+        } else {
+            System.out.println("sorry you lost");
         }
-        return "sorry you lost";
-    }
-    static String getComputerInput(List<String> computerData){
-        Random random = new Random();
-       return computerData.get(random.nextInt(computerData.size()));
+        Scanner input = new Scanner(System.in);
+        System.out.println("you want play again Enter yes");
+        String demo = input.nextLine().toLowerCase();
+        if (demo.equals("yes")) {
+            playAgain();
+        }
+        return "game over";
     }
 
-    public static void main(String[] args) {
+    static void playAgain() {
+        String userInput = getUserInput();
+        getGameResult(userInput, getComputerInput(computerData));
+
+    }
+
+    static String getComputerInput(List<String> computerData) {
+        Random random = new Random();
+        return computerData.get(random.nextInt(computerData.size()));
+    }
+
+    static String getUserInput() {
         Scanner input = new Scanner(System.in);
         System.out.println("choose one from 'rock' , 'paper' , 'scissors' ");
         String userInput = input.nextLine().toLowerCase();
-        List<String> computerData = Arrays.asList("rock", "paper", "scissors");
+        return userInput;
+    }
+
+    public static void main(String[] args) {
+        String userInput = getUserInput();
         if (computerData.contains(userInput)) {
             System.out.println(getGameResult(userInput, getComputerInput(computerData)));
         } else {
